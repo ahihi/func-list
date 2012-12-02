a03f327f62a69e26b0d7f8e1078cb0db9e0eb50b
ahihi <violetmaze@gmail.com>
2012-12-02 10:14:42 +0200

    add some comments

--- FunctionList.hs
+++ FunctionList.hs
@@ -5,54 +5,67 @@ module FunctionList where
 type Index = Integer
 type List a = Index -> Maybe a
 
+-- The empty list
 emptyF :: List a
 emptyF _ = Nothing
 
+-- Construction
 (-:-) :: a -> List a -> List a
 (x -:- xs) 0 = Just x
 (x -:- xs) i = xs (i-1)
 infixr 5 -:-
 
+-- (x, xs) view
 viewF :: List a -> Maybe (a, List a)
 viewF xs = (, xs') `fmap` xs 0
     where
         xs' i = xs (i+1)
 
+-- The first element
 headF :: List a -> Maybe a
 headF = fmap fst . viewF
 
+-- All but the first element
 tailF :: List a -> Maybe (List a)
 tailF = fmap snd . viewF
 
+-- Null predicate
 nullF :: List a -> Bool
 nullF (viewF -> Nothing) = True
 nullF _                  = False
 
+-- Right fold
 foldrF :: (a -> b -> b) -> b -> List a -> b
 foldrF f z = maybe z f' . viewF
     where
         f' (x, xs) = f x (foldrF f z xs)
 
+-- Left fold
 foldlF :: (b -> a -> b) -> b -> List a -> b
 foldlF f = go
     where
         go y (viewF -> Just (x, xs)) = go (f y x) xs
         go y _                       = y
 
+-- Convert to a Prelude list
 fromF :: List a -> [a]
 fromF = foldrF (:) []
 
+-- Convert from a Prelude list
 toF :: [a] -> List a
 toF = foldr (-:-) emptyF
 
+-- Concatenation
 (-+-) :: List a -> List a -> List a
 xs -+- ys = foldrF (-:-) ys xs
 
+-- Map
 mapF :: (a -> b) -> List a -> List b
 mapF f = foldrF f' emptyF
     where
         f' x ys = f x -:- ys
 
+-- Filter
 filterF :: (a -> Bool) -> List a -> List a
 filterF p = foldrF p' emptyF
     where
@@ -60,5 +73,6 @@ filterF p = foldrF p' emptyF
             | p x       = x -:- ys
             | otherwise = ys
 
+-- Reverse
 reverseF :: List a -> List a
 reverseF = foldlF (flip (-:-)) emptyF
